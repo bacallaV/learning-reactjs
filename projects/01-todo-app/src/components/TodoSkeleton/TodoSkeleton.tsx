df1f1eed14492366fd73ede028daa5e37de9e15a
bacallaV <valdohalo3@hotmail.com>
2025-02-17 09:25:35 -0600

    fix: :bug: TodoItem animation overlap issue

--- projects/01-todo-app/src/components/TodoSkeleton/TodoSkeleton.tsx
+++ projects/01-todo-app/src/components/TodoSkeleton/TodoSkeleton.tsx
@@ -2,13 +2,13 @@ import './TodoSkeleton.css'
 
 export default function TodoSkeleton() {
   return (
-    <li className='ti-item'>
-      <div className={`ti-button`}>
+    <li className='tis-item'>
+      <div className={`tis-button`}>
       </div>
 
-      {/* <div className={`ti-label`}></div> */}
+      {/* <div className={`tis-label`}></div> */}
 
-      <div className='ti-button'>
+      <div className='tis-button'>
       </div>
     </li>
   )
