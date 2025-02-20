8a442e49b1cc3a71b6a59c439fe9b8b7389af19f
bacallaV <valdohalo3@hotmail.com>
2025-02-20 09:12:35 -0600

    feat: :sparkles: router install and config

--- projects/02-ecommerce/src/App.tsx
+++ projects/02-ecommerce/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// Routes
+import HomePage from './pages/Home'
+import NotFoundPage from './pages/NotFound';
+import MyAccountPage from './pages/MyAccount';
+import MyOrderPage from './pages/MyOrder';
+import MyOrdersPage from './pages/MyOrders';
+import SignInPage from './pages/SignIn';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/my-account',
+      element: <MyAccountPage />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/my-order',
+      element: <MyOrderPage />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/my-orders',
+      element: <MyOrdersPage />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/sign-in',
+      element: <SignInPage />,
+      errorElement: <NotFoundPage />,
+    },
+    {
+      path: '/*',
+      element: <NotFoundPage />,
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className='text-3xl font-bold underline'>🚀 Hello World!</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ projects/02-ecommerce/src/pages/MyOrder/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyOrderPage() {
+  return (
+    <div>MyOrderPage</div>
+  )
+}
