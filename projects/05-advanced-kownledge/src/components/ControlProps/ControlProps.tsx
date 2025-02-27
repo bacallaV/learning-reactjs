4f55b9ab6d693334ffb18b064715ff0582cdcd86
bacallaV <valdohalo3@hotmail.com>
2025-02-27 10:09:43 -0600

    feat: :sparkles: control props

--- projects/05-advanced-kownledge/src/components/ControlProps/ControlProps.tsx
+++ projects/05-advanced-kownledge/src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,40 @@
-export const ParentComponent = () => {
+import React from 'react'
+
+export function ParentComponent() {
+  const [isToggled, setIsToggled] = React.useState(false);
+
+  function handleToggle(value: boolean) {
+    setIsToggled(value);
+  }
+
   return (
     <div>
+      <p>Valor toggle: {isToggled ? 'ON' : 'OFF'}</p>
+      <Toggle isToggled={isToggled} onToggle={handleToggle} />
     </div>
-  );
-};
+  )
+}
+
+type ToggleProps = {
+  isToggled?: boolean,
+  onToggle?: (value: boolean) => void,
+}
+function Toggle({ isToggled, onToggle }: ToggleProps) {
+  const [value, setValue] = React.useState(isToggled);
+
+  function handleToggle() {
+    setValue(!value);
+
+    if (onToggle) {
+      onToggle(!value);
+    }
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleToggle}>
+        {value ? 'ON' : 'OFF'}
+      </button>
+    </div>
+  )
+}
