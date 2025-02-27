8fdeb90f2c65bbf54f409b0d1eb1772f5830d997
bacallaV <valdohalo3@hotmail.com>
2025-02-27 11:24:46 -0600

    feat: :sparkles: high order components

--- projects/05-advanced-kownledge/src/components/HighOrderComponent/HighOrderComponent.tsx
+++ projects/05-advanced-kownledge/src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,35 @@
-export const ParentComponent = () => {
+import React from 'react'
+
+export function ParentComponent() {
   return (
     <div>
+      <UserWithLoading
+        isLoading={true}
+        name="Valdo de Luna"
+      />
+      <UserWithLoading
+        isLoading={false}
+        name="Valdo Herrera"
+      />
     </div>
+  )
+}
+
+type UserComponentProps = {
+  name: string;
+}
+function UserComponent({ name }: UserComponentProps) {
+  return (
+    <p>Hello, {name}!</p>
   );
-};
+}
+
+const UserWithLoading = withLoading(UserComponent);
+
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props;
+
+    return isLoading ? (<p> Loading...</p>) : <Component {...rest as T} />
+  }
+}
