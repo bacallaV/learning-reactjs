07bd57d46442686b7403596fb396da4ad7dd4069
bacallaV <valdohalo3@hotmail.com>
2025-02-27 17:59:02 -0600

    feat: :sparkles: controlled and uncontrolled inputs

--- projects/05-advanced-kownledge/src/components/ControlledComponent/ControlledComponent.tsx
+++ projects/05-advanced-kownledge/src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,16 @@
-export const ControlledInput = () => {
+import React from 'react'
+
+export function ControlledInput() {
+  const [value, setValue] = React.useState('');
+
+  function onChangeValue(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
   return (
     <div>
+      <input type="text" name="cart" id="cart" value={value} onChange={onChangeValue} placeholder='Ingresa tu cupón' />
+      <p>Cupón ingresado: {value}</p>
     </div>
-  );
-};
+  )
+}
--- projects/05-advanced-kownledge/src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ projects/05-advanced-kownledge/src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,16 @@
-export const UncontrolledInput = () => {
+import React from 'react'
+
+export function UncontrolledInput() {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  function onAddToCart() {
+    alert(inputRef.current?.value);
+  }
+
   return (
     <div>
+      <input type="text" name="cart" id="cart" placeholder='Ingresa tu producto Ej. Manzana' ref={inputRef} />
+      <button type="button" onClick={onAddToCart}>Agregar al carrito</button>
     </div>
-  );
-};
+  )
+}
