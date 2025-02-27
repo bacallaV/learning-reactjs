343c9f2f36239114be9622266721f4685b553eff
bacallaV <valdohalo3@hotmail.com>
2025-02-27 16:10:39 -0600

    feat: :sparkles: container and presenter

--- projects/05-advanced-kownledge/src/components/ContainerPresenter/DataContainer.tsx
+++ projects/05-advanced-kownledge/src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,40 @@
+import React from "react";
+import { DataItem } from "./DataItem";
+import { DataPresenter } from "./DataPresenter";
+
 export const DataContainer = () => {
+  const [data, setData] = React.useState<Array<DataItem>>([]);
+  const [state, setState] = React.useState<'loading' | 'success' | 'error'>('loading');
+
+  React.useEffect(() => {
+    setState('loading');
+
+    fetch('/data/data.json')
+      .then(
+        res => res.json()
+      )
+      .then(
+        data => {
+          setData(data as DataItem[]);
+          setState('success');
+        }
+      )
+      .catch(
+        () => setState('error'),
+      );
+  }, []);
+
+  if (state === 'loading') {
+    return (
+      <p>Cargando...</p>
+    );
+  } else if(state === 'error') {
+    return (
+      <p>Oops! Algo salió mal</p>
+    );
+  }
+
   return (
-    <div>
-    </div>
+    <DataPresenter data={data} />
   );
 };
--- projects/05-advanced-kownledge/src/components/ContainerPresenter/DataPresenter.tsx
+++ projects/05-advanced-kownledge/src/components/ContainerPresenter/DataPresenter.tsx
@@ -1,6 +1,17 @@
-export const DataPresenter = () => {
+import { DataItem } from "./DataItem";
+
+type DataPresenterProps = {
+  data: Array<DataItem>
+}
+export function DataPresenter({ data }: DataPresenterProps) {
   return (
-    <>
-    </>
+    <div>
+      {data.map((item) => (
+        <>
+          <img src={item.image} alt={item.name} />
+          <em>{item.description}</em>
+        </>
+      ))}
+    </div>
   );
 };
