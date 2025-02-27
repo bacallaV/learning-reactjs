3d2d3cdf561589566aa58a2843dcab324361375e
bacallaV <valdohalo3@hotmail.com>
2025-02-27 13:23:08 -0600

    feat: :sparkles: custom hook

--- projects/05-advanced-kownledge/src/components/CustomHooks/CustomHooks.tsx
+++ projects/05-advanced-kownledge/src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,42 @@
-const UserComponent = () => {
+import React from "react";
+
+type UserComponentProps = {
+  name: string;
+}
+function UserComponent({ name }: UserComponentProps) {
+  const { isLoading, setIsLoading } = useLoading(false);
+
+  function toggleLoading() {
+    setIsLoading(!isLoading);
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <div>
+      <p>Hello, {name} !</p>
+      <button type="button" onClick={toggleLoading}>Toggle loading</button>
     </div>
   );
 };
 
-export const ParentComponent = () => {
-  return <UserComponent />;
-};
+export function ParentComponent() {
+  return (
+    <UserComponent name="Valdo de Luna" />
+  )
+}
+
+function useLoading(initialLoading: boolean = false) {
+  const [isLoading, setIsLoading] = React.useState(initialLoading);
+
+  return {
+    isLoading,
+    setIsLoading,
+  }
+}
